3470312af23c13883d0a4e4e067aa587da14bd4f
Franz Brummer <linuxfranz@users.noreply.github.com>
2014-12-20 11:21:23 +0100

    Added subjects. Rearranged cols.

--- run_analysis.R
+++ run_analysis.R
@@ -18,14 +18,23 @@ cols <- c(rep(cols_3d,5),   # 200
           rep(cols_2d,4),   # 554
           rep("NULL", 7))     # 561
 cols_labels <- cols == "numeric"
+# read and merge subjects
+subjects <- read.csv(paste(dir, "test/subject_test.txt", sep="/"), sep="", header=FALSE)
+subjects <- rbind(subjects,read.csv(paste(dir, "train/subject_train.txt", sep="/"), sep="", header=FALSE))
+names(subjects) <- "subjects"
+# read and merge features
 data_x <- read.csv(paste(dir, "test/X_test.txt", sep="/"), sep="", header=FALSE, colClasses=cols)
 data_y <- read.csv(paste(dir, "test/y_test.txt", sep="/"), sep="", header=FALSE)
 data_x <- rbind(data_x, read.csv(paste(dir, "train/X_train.txt", sep="/"), sep="", header=FALSE, colClasses=cols))
 data_y <- rbind(data_y, read.csv(paste(dir, "train/y_train.txt", sep="/"), sep="", header=FALSE))
+# read and merge activities, delete activitynr
 names(data_y) <- "activitynr"
 activities <- read.csv(paste(dir, "activity_labels.txt", sep="/"), sep="", header=FALSE, col.names=c("nr", "activity"))
 data_y <- merge(data_y, activities, by.x="activitynr", by.y="nr")
+data_y$activitynr <- NULL
+# read and set column labels (features)
 feats <- read.csv(paste(dir, "features.txt", sep="/"),
                   sep="", header=FALSE, colClasses=c("NULL","character"))
 names(data_x) <- feats[cols_labels,1]
-data <- cbind(data_x, data_y)
+# merge subjects, data_x and data_y
+data <- cbind(subjects, data_y, data_x)
